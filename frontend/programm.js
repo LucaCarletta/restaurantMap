50e990977d4d1cd98bcbb85ccdac8989d6b5aed7
kleiber <kleiber@adobe.com>
2021-10-24 17:57:01 +0200

    responsive

--- frontend/programm.js
+++ frontend/programm.js
@@ -2,9 +2,9 @@ document.addEventListener("DOMContentLoaded", function (event) {
     document.getElementById("submitbutton").onclick = () => {
 
         var img = document.createElement("img");
-
-        img.src = "image.png";
-        var src = document.getElementById("x");
+        img.classList.add("img-fluid")
+        img.src = "bsp/Map.png";
+        var src = document.getElementById("image");
 
         src.appendChild(img);
         console.log("Hello World")
