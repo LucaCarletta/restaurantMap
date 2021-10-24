4d802336347111e1015862190e22d263b007a02c
kleiber <kleiber@adobe.com>
2021-10-24 22:36:01 +0200

    Finished Frontend

--- frontend/programm.js
+++ frontend/programm.js
@@ -1,12 +1,35 @@
+//Wartet bis Dom geladen ist
 document.addEventListener("DOMContentLoaded", function (event) {
     document.getElementById("submitbutton").onclick = () => {
+        var src = document.getElementById("image");
+        var prices = document.getElementsByName('preis');
+        //Default Price Option
+        var price = 1
 
+
+        // Get Value from Radio Buttons
+        for (let i of prices) {
+            if (i.checked) {
+                price = i.value
+            }
+        }
+
+        //Erdstellt Bild Element
         var img = document.createElement("img");
         img.classList.add("img-fluid")
-        img.src = "bsp/Map.png";
-        var src = document.getElementById("image");
 
-        src.appendChild(img);
-        console.log("Hello World")
+        //macht API Request mit spezifischem Preis Bekommt Blob zurück
+        fetch(`http://localhost:5000?price=${price}`)
+            .then(response => response.blob())
+            .then(function (myBlob) {
+                //Erstellt URL von einem Blob Element
+                var objectURL = URL.createObjectURL(myBlob);
+                img.src = objectURL;
+                //fügt Bild dem Div hinzu
+                src.hasChildNodes() ? src.replaceChild(img, src.childNodes[0]) : src.appendChild(img)
+            })
+            .catch(error => console.error(error))
+
+
     }
 });
