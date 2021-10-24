f94d4f5a8e91afdb4ea798ba1c952ca7f64e412d
LucaCarletta <54622880+LucaCarletta@users.noreply.github.com>
2021-10-24 17:32:54 +0200

    search bar and picture

--- frontend/programm.js
+++ frontend/programm.js
@@ -1,5 +1,12 @@
 document.addEventListener("DOMContentLoaded", function (event) {
     document.getElementById("submitbutton").onclick = () => {
+
+        var img = document.createElement("img");
+
+        img.src = "image.png";
+        var src = document.getElementById("x");
+
+        src.appendChild(img);
         console.log("Hello World")
     }
 });
